8a316c9682b307cf0bfdefbbf7d22ca321280f03
childofukraine <98478383+childofukraine@users.noreply.github.com>
2023-03-12 18:46:52 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -7,13 +7,11 @@ import { errorHandler } from "./utils/error_handler";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-const corsOptions = {
-    origin: `http://localhost:${PORT}` , 
-    credentials:true,           
-    optionSuccessStatus:200
-}
 
-app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
 app.use("/", router);
 app.use(errorHandler);
 
