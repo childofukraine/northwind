1c838f5825984d82d45056d3688c97dbe4936e90
childofukraine <98478383+childofukraine@users.noreply.github.com>
2023-03-12 19:59:03 +0200

    add origin

--- src/server.ts
+++ src/server.ts
@@ -10,12 +10,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors())
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors({
+  origin: 'http://localhost:3000/'
+}));
 app.use("/", router);
 app.use(errorHandler);
 
