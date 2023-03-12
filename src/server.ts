de228b1d4b4d0df93232f909877bd162980032eb
childofukraine <98478383+childofukraine@users.noreply.github.com>
2023-03-12 20:06:58 +0200

    app use

--- src/server.ts
+++ src/server.ts
@@ -10,9 +10,16 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors({
-  origin: 'http://localhost:3000/'
-}));
+app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://localhost:3000");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+  })
 app.use("/", router);
 app.use(errorHandler);
 
