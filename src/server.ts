3b52cce957adcc78212d07d327d930c6db29c79b
childofukraine <98478383+childofukraine@users.noreply.github.com>
2023-03-12 17:44:53 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -8,8 +8,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use("/", router);
 app.use(cors());
+app.use("/", router);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
