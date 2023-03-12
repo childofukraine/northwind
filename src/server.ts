bb80c7f4afa58e948892f13608e0d7edc48e4b9a
childofukraine <98478383+childofukraine@users.noreply.github.com>
2023-03-12 19:50:15 +0200

    header updt

--- src/server.ts
+++ src/server.ts
@@ -10,31 +10,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors({
-    origin: [
-      "http://192.168.0.157:3000",
-      "http://213.111.67.182:5173",
-      "http://localhost:5173",
-      "http://localhost:3000",
-    ],
-    methods: ["HEAD", "OPTIONS", "POST", "GET", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Uppy-Versions",
-      "Accept",
-      "x-requested-with",
-      "Access-Control-Allow-Origin",
-    ],
-    exposedHeaders: [
-      "Access-Control-Allow-Headers",
-      "Access-Control-Allow-Origin",
-    ],
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-  }));
-
+app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use("/", router);
 app.use(errorHandler);
 
