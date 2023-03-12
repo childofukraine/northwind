8b3a01ef348e4ca53a423784dfef4e1db21af238
childofukraine <98478383+childofukraine@users.noreply.github.com>
2023-03-12 20:29:08 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import router from "./router/router";
+// import router from "./router/router";
 import { errorHandler } from "./utils/error_handler";
 
 dotenv.config();
@@ -11,16 +11,9 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://localhost:3000");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-  })
-app.use("/", router);
+// app.use("/", router);
+
+
 app.use(errorHandler);
 
 app.listen(PORT, () => {
