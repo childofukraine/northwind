41b64dd136970bc133a25f3e9353e06c2ebb70c6
childofukraine <98478383+childofukraine@users.noreply.github.com>
2023-03-12 20:31:27 +0200

    add router to server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 // import router from "./router/router";
+import { Controller } from "./controller/controller"
 import { errorHandler } from "./utils/error_handler";
 
 dotenv.config();
@@ -11,6 +12,20 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+
+const router = express.Router();
+router.get("/suppliers", Controller.suppliers);
+router.get("/suppliers/:supplierId", Controller.suppliersIndexed);
+router.get("/products", Controller.products);
+router.get("/products/:productId", Controller.productsIndexed);
+router.get("/orders", Controller.orders);
+router.get("/orders/:orderId", Controller.ordersIndexed);
+router.get("/employees", Controller.employees);
+router.get("/employees/:employeeId", Controller.employeesIndexed);
+router.get("/customers", Controller.customers);
+router.get("/customers/:customerId", Controller.customersIndexed);
+router.get("/search-products/:keyword", Controller.searchProducts);
+router.get("/search-customers/:keyword", Controller.searchCustomers);
 // app.use("/", router);
 
 
